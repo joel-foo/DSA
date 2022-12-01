636e130b561eea1923bb596a7eac2ce249b7ce87
joel-foo <joelfootaimeng@gmail.com>
2022-12-02 01:02:20 +0800

    edit

---
+++ Dynamic Programming/max_subarray_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_slice = 0
+        max_sum = float('-inf')
+        for num in nums:
+            max_slice = max(num, max_slice + num)
+            print(max_slice)
+            max_sum = max(max_sum, max_slice)
+        return max_sum
